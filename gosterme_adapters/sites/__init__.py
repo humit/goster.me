fccdd9e8c8a67f3cac4c577ceef97314805b5ba5
humit <humite@gmail.com>
2026-08-13 03:37:04 +0300

    Extract ilk-okul.com site adapter (#46)
    Part of #9.

--- gosterme_adapters/sites/__init__.py
+++ gosterme_adapters/sites/__init__.py
@@ -4,8 +4,10 @@ from .ilkokul_akademi import (
     IlkokulAkademiGithubEmbedAdapter,
     IlkokulAkademiNativeAdapter,
 )
+from .ilk_okul import IlkOkulNativeAdapter
 
 __all__ = [
     "IlkokulAkademiGithubEmbedAdapter",
     "IlkokulAkademiNativeAdapter",
+    "IlkOkulNativeAdapter",
 ]
